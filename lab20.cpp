b8b633ac7bd9a2975f5f0ba0402744e2dcb09d0e
llt3055 <llt30555@gmail.com>
2026-03-12 20:21:26 -0700

    continue to delete the unecessary part

--- lab20.cpp
+++ lab20.cpp
@@ -63,28 +63,24 @@ int main() {
 
     //creating pointer to first chair object
     Chair *chairPtr = new Chair;
-    chairPtr->setLegs(4);
-    chairPtr->setPrices(121.21, 232.32, 414.14);
     chairPtr->print();
+    delete chairPtr;
+    chairPtr = nullptr;
+    
 
     //creating dynamic chair object with constructor
     double p[] = {525.25, 434.34, 252.52};
     Chair *livingChair = new Chair(3, p);
-    livingChair->setPrices(525.25, 434.34, 252.52);
-    livingChair->print();
     delete livingChair;
     livingChair = nullptr;
 
     //creating dynamic array of chair objects
     Chair *collection = new Chair[SIZE];
-    collection[0].setLegs(4);
-    collection[0].setPrices(441.41, 552.52, 663.63);
-    collection[1].setLegs(4);
-    collection[1].setPrices(484.84, 959.59, 868.68);
-    collection[2].setLegs(4);
-    collection[2].setPrices(626.26, 515.15, 757.57);
+
     for (int i = 0; i < SIZE; i++)
         collection[i].print();
     
+    delete[] collection;
+    collection = nullptr;
     return 0;
 }
